d6703e3cd7f9496dcbffbc4dcb817e19c4f63ce1
Rabin Chudali <nohorasinchan888@gmail.com>
2025-06-12 08:17:10 +0545

    Initial commit

---
+++ types/index.ts
@@ -0,0 +1,14 @@
+export interface MoodEntry {
+  id: string
+  mood: string
+  note: string
+  date: string
+}
+
+export interface JournalEntryType {
+  id: string
+  title: string
+  content: string
+  mood: string
+  date: string
+}
